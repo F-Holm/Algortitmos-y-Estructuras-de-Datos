4aa2fc8dd85d5ee6024d30f954640d59deef14b3
F-Holm <moshishafcdodgecharger@gmail.com>
2025-04-30 11:54:54 -0300

    Unidad i, Ejercicio 24 incompleto

---
+++ Unidad 1/Ejercicio 24.cpp
@@ -0,0 +1,22 @@
+// Unidad 1 - Ejercicio 24
+#include <iostream>
+#include <iomanip>
+
+typedef char str20[21];
+
+using namespace std;
+
+struct venta {
+    ushort codVen,
+           cant;
+    str20  descrip; // cadena de caracteres al estilo Leng. C
+    float  preUni;
+};
+
+int main() {
+    cout << fixed << setprecision(2);
+
+    cout << a << endl;
+    cout << setfill('0') << setw(8) << a << endl;
+    return 0;
+}
