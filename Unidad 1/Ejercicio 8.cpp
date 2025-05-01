c8160b5ad8675b5241e7faea971f951827d71327
Holm <you@example.com>
2025-05-01 17:06:40 -0300

    Ej. 8

---
+++ Unidad 1/Ejercicio 8.cpp
@@ -0,0 +1,12 @@
+// Unidad 1 - Ejercicio 8
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    unsigned int n, sum = 0;
+    cout << "Ingrese un número natural: ";
+    cin >> n;
+    cout << "La sumatoria de todos los números naturales hasta " << n << " es: " << n/2.0*(n+1) << endl;
+    return 0;
+}
