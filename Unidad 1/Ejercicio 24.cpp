d8080c52ec1853333544f5aab619f05557d08e0b
Holm <you@example.com>
2025-05-07 15:47:47 -0300

    Se escribe el registro de ventas Ej 24

--- Unidad 1/Ejercicio 24.cpp
+++ Unidad 1/Ejercicio 24.cpp
@@ -15,8 +15,17 @@ struct venta {
     float  preUni;
 };
 
+void divisorHorizontal(){
+    string str = "";
+    for (unsigned short i = 0;i < 60;i++)
+        str += '-';
+    cout << str << endl;
+}
+
 venta crearVenta() {
     venta v;
+    cout << "Ingrese los datos de la venta (0 = SALIR):\n\n";
+
     cout << "codigo: ";
     cin >> v.codVen;
     if (v.codVen == 0)
@@ -28,28 +37,25 @@ venta crearVenta() {
     cout << "descripcion: ";
     cin.ignore();
     cin.get(v.descrip, 21);
+    cin.ignore(100, '\n');
 
     cout << "precio: ";
     cin >> v.preUni;
 
+    divisorHorizontal();
     return v;
 }
 
-void divisorHorizontal(){
-    string str = "";
-    for (unsigned short i = 0;i < 60;i++)
-        str += '—';
-    cout << str << endl;
-}
-
 void emitirVenta(venta v, ofstream &ventas){
-    ventas << setfill('0') << setw(3) << v.codVen << "\t|\t" <<
-    setfill('0') << setw(4) << v.cant << "\t|\t" <<
-    setfill(' ') << setw(20) << v.descrip << "\t|\t" <<
-    setfill('0') << setw(7) << v.preUni << endl;
+    ventas << setfill('0') << setw(3) << v.codVen << "  |  " <<
+            setfill('0') << setw(4) << v.cant << "  |  " <<
+    left << setfill(' ') << setw(20) << v.descrip << "  |  " <<
+    right << setfill('0') << setw(8) << v.preUni << '\n';
 }
 
-string* leerRegistroVentas(ifstream &iRegistro){
+string* leerRegistroVentas(ifstream &iRegistro, unsigned short size){
+    string* str = new string[size];
+    return str;
 }
 
 void reordenar(string* str_ventas, unsigned short size, ofstream &ventas){
@@ -63,23 +69,24 @@ int main() {
     venta v;
 
     ofstream ventas("RegistroVentas.txt");
-    ofstream salida("Salida.txt");
+    //ofstream salida("Salida.txt");
 
-    cout << "ventas agrupadas por vendedor" << endl;
-    cout << "ingrese las ventas: " << endl << endl;;
+    cout << "ingrese las ventas: " << endl;
+    divisorHorizontal();
 
     v = crearVenta();
+    ventas << fixed << setprecision(2);
     while (v.codVen != 0){
         emitirVenta(v, ventas);
         contador++;
         v = crearVenta();
     }
 
-    ifstream iRegistro("RegistroVentas.txt");
+    /*ifstream iRegistro("RegistroVentas.txt");
     
-    cout << fixed << setprecision(2);
+    
     float a = 101.1;
     cout << a << endl;
-    cout << setfill('0') << setw(8) << a << endl;
+    cout << setfill('0') << setw(8) << a << endl;*/
     return 0;
 }
