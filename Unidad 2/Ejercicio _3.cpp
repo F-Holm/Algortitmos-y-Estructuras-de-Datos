19eba2d94028877a1b128259f0fcb504f8cfd2f9
F-Holm <moshishafcdodgecharger@gmail.com>
2025-05-14 11:33:49 -0300

    ejercicio _3

---
+++ Unidad 2/Ejercicio _3.cpp
@@ -0,0 +1,14 @@
+// Unidad 2 - Ejercicio _3
+#include <iostream>
+
+using namespace std;
+
+char LowCase(char car) {
+  if (car >= 'A' && car <= 'Z') car += 'a' - 'A';
+  return car;
+}
+
+int main() {
+  cout << "'A' -> " << LowCase('A') << endl;
+  return 0;
+}
