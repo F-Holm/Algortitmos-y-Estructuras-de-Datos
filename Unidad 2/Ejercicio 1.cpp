a55fceb544fbad80f801282216760ad204f4c285
F-Holm <moshishafcdodgecharger@gmail.com>
2025-05-14 11:54:08 -0300

    fin clase

---
+++ Unidad 2/Ejercicio 1.cpp
@@ -0,0 +1,15 @@
+// Unidad 2 - Ejercicio 1
+#include <iostream>
+
+using namespace std;
+
+struct Ingresante{
+  char legajo[6];
+  char apellido_nombre[31];
+  unsigned short division;
+};
+
+int main() {
+  ;
+  return 0;
+}
