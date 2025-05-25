7a1d1745db7d4235a5af87fc8172fd479996f3f6
Holm <you@example.com>
2025-05-25 19:53:48 -0300

    funciona e.exe

--- setup.bat
+++ setup.bat
@@ -1,7 +1,7 @@
 @echo off
 clang++ -std=c++23 -Wall -Wextra -Wpedantic -O3 -DNDEBUG ./src/build.cpp -o build.exe
-clang++ -std=c++23 -Wall -Wextra -Wpedantic -g -O0 -DDEBUG ./src/ejecutar.cpp -o r.exe
+clang++ -std=c++23 -Wall -Wextra -Wpedantic -g -O0 -DDEBUG ./src/ejecutar.cpp -o e.exe
 REM clang++ -std=c++23 -Wall -Wextra -Wpedantic -g -O0 -DDEBUG ./src/compilar.cpp -o c.exe
-clang++ -std=c++23 -Wall -Wextra -Wpedantic -O3 -DNDEBUG ./src/clean.cpp -o g.exe
+clang++ -std=c++23 -Wall -Wextra -Wpedantic -O3 -DNDEBUG ./src/limpiar.cpp -o l.exe
 clang++ -std=c++23 -Wall -Wextra -Wpedantic -O3 -DNDEBUG ./src/formatear.cpp -o f.exe
 REM clang++ -std=c++23 -Wall -Wextra -Wpedantic -g -O0 -DDEBUG ./src/nuevo.cpp -o n.exe
