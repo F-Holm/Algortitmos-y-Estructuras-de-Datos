0c1c3be6ecdb09bf171f3c3b01b2425383bc0a1f
Holm <you@example.com>
2025-05-25 21:40:33 -0300

    terminé setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+clang++ -std=c++23 -Wall -Wextra -Wpedantic -O3 -DNDEBUG ./src/build.cpp -o build
+clang++ -std=c++23 -Wall -Wextra -Wpedantic -O3 -DNDEBUG ./src/ejecutar.cpp -o e
+clang++ -std=c++23 -Wall -Wextra -Wpedantic -O3 -DNDEBUG ./src/compilar.cpp -o c
+clang++ -std=c++23 -Wall -Wextra -Wpedantic -O3 -DNDEBUG ./src/limpiar.cpp -o l
+clang++ -std=c++23 -Wall -Wextra -Wpedantic -O3 -DNDEBUG ./src/formatear.cpp -o f
+clang++ -std=c++23 -Wall -Wextra -Wpedantic -O3 -DNDEBUG ./src/nuevo.cpp -o n
