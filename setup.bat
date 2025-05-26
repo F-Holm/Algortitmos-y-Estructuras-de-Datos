7a37775a8a350ac10546af554ebbd7d4b51b82d0
Holm <you@example.com>
2025-05-25 21:36:16 -0300

    nuevo.cpp

--- setup.bat
+++ setup.bat
@@ -1,7 +1,7 @@
 @echo off
 clang++ -std=c++23 -Wall -Wextra -Wpedantic -O3 -DNDEBUG ./src/build.cpp -o build.exe
 clang++ -std=c++23 -Wall -Wextra -Wpedantic -O3 -DNDEBUG ./src/ejecutar.cpp -o e.exe
-clang++ -std=c++23 -Wall -Wextra -Wpedantic -g -O0 -DDEBUG ./src/compilar.cpp -o c.exe
+clang++ -std=c++23 -Wall -Wextra -Wpedantic -O3 -DNDEBUG ./src/compilar.cpp -o c.exe
 clang++ -std=c++23 -Wall -Wextra -Wpedantic -O3 -DNDEBUG ./src/limpiar.cpp -o l.exe
 clang++ -std=c++23 -Wall -Wextra -Wpedantic -O3 -DNDEBUG ./src/formatear.cpp -o f.exe
 REM clang++ -std=c++23 -Wall -Wextra -Wpedantic -g -O0 -DDEBUG ./src/nuevo.cpp -o n.exe
---
+++ src/nuevo.cpp
@@ -0,0 +1,50 @@
+#include <fstream>
+#include <string>
+#include <filesystem>
+
+#include "ejecutar.hpp"
+#include "formatear.hpp"
+
+using std::string;
+
+namespace fs = std::filesystem;
+
+inline fs::path Nombre(const string& unidad, const string& ejercicio);
+inline string Archivo(const string& unidad, const string& ejercicio);
+
+
+int main(int argc, char* argv[]) {
+  string unidad = argc >= 3 ? argv[1] : "";
+  string ejercicio = argc >= 3 ? argv[2] : "";
+
+  while (!EsValido(unidad) || !EsValido(ejercicio)) {
+    std::cout << "Unidad: ";
+    std::cin >> unidad;
+    std::cout << "Ejercicio: ";
+    std::cin >> ejercicio;
+    std::cout << std::endl;
+  }
+
+  std::ofstream archivo(Nombre(unidad, ejercicio));
+  archivo << Archivo(unidad, ejercicio);
+  archivo.close();
+  
+  Formatear(true, false);
+}
+
+inline fs::path Nombre(const string& unidad, const string& ejercicio){
+    return fs::path(string("./unidad_") + unidad + string("/ejercicio_") + ejercicio + string(".cpp"));
+}
+
+inline string Archivo(const string& unidad, const string& ejercicio){
+    return R"(// Unidad )" + unidad + R"( - Ejercicio )" + unidad + R"(
+    #include <iostream>
+
+    using namespace std;
+
+    int main() {
+        ;
+        return 0;
+    }
+    )";
+}
